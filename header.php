a4bf29baa9afac4f58367ed3d8b767dc834954df
youssef bouhlal <pro.youssef.bouhlal@gmail.com>
2021-02-10 13:45:57 +0100

    add mini cart

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 
     <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?> >
+<body <?php body_class('woocommerce'); ?> >
     <?php wp_body_open(); ?>
     <div id="page" class="site">
         <header class="site-header pt-3 pb-3" role="banner">
@@ -33,12 +33,12 @@
                                 <a href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ); ?>" class="nav-link btn btn-outline-success btn-sm pt-0 pb-0"><?php esc_html_e( 'Login / Register', 'ucef-woo' ); ?></a>
                             <?php endif; ?>
                             <div class="cart position-relative pl-2 pr-1">
-                                <a href="<?php echo esc_url( wc_get_cart_url() ); ?>">
+                                <button type="button" class="btn btn-link btn-outline-light p-0" data-toggle="modal" data-target="#mini-cart">
                                     <span class="cart-icon">
                                         <?php ucef_woo_svg_inline( 'cart' ); ?>
                                     </span>
-                                    <span class="items badge badge-pill badge-dark position-absolute"><?php echo esc_html( WC()->cart->get_cart_contents_count() ); ?></span>
-                                </a>
+                                    <span class="cart-count items badge badge-pill badge-dark position-absolute"><?php echo esc_html( WC()->cart->get_cart_contents_count() ); ?></span>
+                                </button>
                             </div>
                         <?php endif; ?>
                         <a class="ml-2" data-toggle="collapse" href="#searchForm" role="button" aria-expanded="false" aria-controls="searchForm">
@@ -48,7 +48,6 @@
                 </div>
             </section><!-- .account 'show if woocommerce activated' -->
             
-
             <section class="collapse" id="searchForm">
                 <div class="container">
                     <?php get_search_form(); ?>
